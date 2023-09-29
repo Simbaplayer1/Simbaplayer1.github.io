38ff4741b302ce0391ea749a534ef11f732c674f
Simbaplayer1 <juliusatliva@gmail.com>
2023-09-29 22:18:27 +0200

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -2,12 +2,20 @@ function toggleDarkMode() {
   const body = document.body;
   const isDarkMode = body.classList.toggle("darkMode");
   localStorage.setItem("darkMode", isDarkMode ? "true" : "false");
+
+  const btnKnob = document.querySelector(".btn-knob");
+  if (isDarkMode) {
+    btnKnob.style.transform = "translateX(2.5rem)";
+  } else {
+    btnKnob.style.transform = "translateX(0)";
+  }
 }
 
-// Check if dark mode preference is set in local storage and apply it
 document.addEventListener("DOMContentLoaded", function () {
   const isDarkMode = localStorage.getItem("darkMode") === "true";
   if (isDarkMode) {
     document.body.classList.add("darkMode");
+    const btnKnob = document.querySelector(".btn-knob");
+    btnKnob.style.transform = "translateX(2.5rem)";
   }
 });
